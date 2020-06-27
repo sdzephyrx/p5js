32bba08cc731f69ac7a008a4a2330fc5e960c359
Bruno Lima <sdzephyrx@gmail.com>
2020-06-27 11:25:20 -0300

    Correcao do delay de spawn

--- sketch.js
+++ sketch.js
@@ -42,8 +42,8 @@ function setup() {
   pontuacao= new Pontuacao();
   personagem= new Personagem(matrizPersonagem, imagemPersonagem, 0,30,220,270,220,270);
   const inimigo= new Inimigo(matrizInimigo, imagemInimigo, width-52, 30, 104, 104,104, 104, 10, 100);
-  const inimigoGrande= new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width*2, 0, 400, 400, 350, 350,10,500);
-  const inimigoVoador= new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width*1.5, height*0.5, 200, 150, 180, 125,10,250);
+  const inimigoGrande= new Inimigo(matrizInimigoGrande, imagemInimigoGrande, width*2, 0, 400, 400, 350, 350,10,1500);
+  const inimigoVoador= new Inimigo(matrizInimigoVoador, imagemInimigoVoador, width*1.5, height*0.5, 200, 150, 180, 125,10,1000);
   
   inimigos.push(inimigo);
   inimigos.push(inimigoGrande);
